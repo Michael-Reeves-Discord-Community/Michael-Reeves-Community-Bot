0956832aa77a037426a94beb1e6e0bbb03423e28
fccloud42 <fccloud420@outlook.com>
2020-06-12 14:02:17 -0400

    twitter stuff by clubpenguin

--- Social Media Integration/twitter.py
+++ Social Media Integration/twitter.py
@@ -1,27 +1,36 @@
 import tweepy as tpy #the library that interfaces with the Twitter API
+import os
 
 
 class TwitterBot():
-    def __init__(self, akey, asecret, apikey, apisecret, targetUser):
-        self.akey = akey
-        self.asecret = asecret
-        self.apikey = apikey
-        self.apisecret = apisecret
-        self.api = self.auth()
-        self.latestTweet = self.getLatestTweet(targetUser)
+	def __init__(self, akey, asecret, apikey, apisecret, targetUser):
+		self.akey = akey
+		self.asecret = asecret
+		self.apikey = apikey
+		self.apisecret = apisecret
+		self.api = self.auth()
+		self.latestTweet = self.getLatestTweet(targetUser)
+		self.saveLatestTweet(self.latestTweet)
 
-    def auth(self):
-        try:
-            auth = tpy.OauthHandler(self.apikey, self.apisecret)
-            auth.set_access_token(self.akey, self.asecret)
-            return tpy.API(auth) 
-        except TweepError:
-            raise ("Check your credentials dumbass. Imagine being such a poo poo") 
+	def auth(self):
+		try:
+			auth = tpy.OauthHandler(self.apikey, self.apisecret)
+			auth.set_access_token(self.akey, self.asecret)
+			return tpy.API(auth) 
+		except TweepError:
+			raise ("Check your credentials dumbass. Imagine being such a poo poo") 
 
-    def getLatestTweet(self, user):
-        tweet = self.api.user_timeline(id = user, count = 1)[0]
-        return tweet.text
+	def getLatestTweet(self, user):
+		tweet = self.api.user_timeline(id = user, count = 1)[0]
+		return tweet.text
 
+	def saveLatestTweet(self, tweet):
+		try: os.remove("latesttweet.txt")
+		except: pass 
+		
+		latest_tweet = open("lattesttweet.txt", "w")
+		latest_tweet.write(tweet)
+		latest_tweet.close()
 
 if __name__ == "__main__":
-	TwitterBot(0, 0, 0, 0, 0) 
+	tweety = TwitterBot(0, 0, 0, 0, 0)
--- cogs/social_stuff.py
+++ cogs/social_stuff.py
@@ -1,2 +1,11 @@
 import discord 
-from discord.ext import commands 
+from discord.ext import commands
+ 
+class Socials(commands.Cog):
+	def __init__(self, bot):
+		self.bot = bot
+		self.tweet = open("")
+	
+	@commands.command()
+	async def twitter(self, ctx):
+		await ctx.send(self.tweet)
