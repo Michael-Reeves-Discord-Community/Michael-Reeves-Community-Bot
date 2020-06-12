66ce2be6d33affcf9a35e7ea82ba924d75fb5aaf
fccloud42 <fccloud420@outlook.com>
2020-06-12 14:39:56 -0400

    Test twitter stuff
    Just a placeholder for twitter API until keys are stored securely.

--- cogs/social_stuff.py
+++ cogs/social_stuff.py
@@ -4,7 +4,7 @@ from discord.ext import commands
 class Socials(commands.Cog):
 	def __init__(self, bot):
 		self.bot = bot
-		self.tweet = open("")
+		self.tweet = "tweets don't work yet. darn. michael's last tweet was something about turtles hahhahhaa"
 	
 	@commands.command()
 	async def twitter(self, ctx):
