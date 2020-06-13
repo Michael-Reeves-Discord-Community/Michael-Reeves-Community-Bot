c43462520fd8b0ea8621d22ea630241e05f94ba4
Filip Grujoski <59372427+Fark1@users.noreply.github.com>
2020-06-13 14:38:33 +0200

    fixing some small fucky wuckys

--- cogs/utils/crack.py
+++ cogs/utils/crack.py
@@ -3,13 +3,13 @@ import collections
 from collections import namedtuple
 
 def get(file):
-	try:
-		with open(file , encoding='utf-8') as data:
-			return json.load(data, object_hook= lambda d: namedtuple('X', d.keys())(*d.values()))
-	except AttributeError:
-		raise AttributeError('unknown argument')
-	except FileNotFoundError:
-		raise FileNotFoundError("json wasent found dips shit ")
+    try:
+        with open(file, encoding='utf8') as data:
+            return json.load(data, object_hook=lambda d: namedtuple('X', d.keys())(*d.values()))
+    except AttributeError:
+        raise AttributeError("unkown argument")
+    except FileNotFoundError:
+        raise FileNotFoundError("a json file wasent found dipshit")
 
 
 def insta_crack(*, numbers=False, stuff=False, things=None, first: int = None, second: int = None, ):
